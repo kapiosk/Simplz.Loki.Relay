af0f4032c4d7d020a69f9fcecd81732041ddeb7c
kapiosk <29824083+kapiosk@users.noreply.github.com>
2023-01-12 12:55:29 +0200

    .

--- Functions.cs
+++ Functions.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Simplz.Loki.Relay.Models;
 using MimeKit;
-using System.Threading;
 using System;
 using System.Linq;
 
@@ -18,8 +17,7 @@ namespace Simplz.Loki.Relay
         [FunctionName("PostMe")]
         public static async Task<IActionResult> PostMe(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
-            ILogger log,
-            CancellationToken token)
+            ILogger log)
         {
             string requestBody = "";
             try
@@ -27,15 +25,15 @@ namespace Simplz.Loki.Relay
                 requestBody = await new StreamReader(req.Body).ReadToEndAsync();
                 var message = System.Text.Json.JsonSerializer.Deserialize<SQSMessage>(requestBody);
 
-                var msg = await MimeMessage.LoadAsync(new MemoryStream(Convert.FromBase64String(message.Content)), token);
-                log.LogWarning("{HtmlBody} {Label} {Name}", msg.HtmlBody ?? msg.TextBody, msg.Subject, msg.To.Mailboxes.FirstOrDefault()?.Address ?? "Anon");
+                var msg = await MimeMessage.LoadAsync(new MemoryStream(Convert.FromBase64String(message.Content)));
+                log.LogWarning("{HtmlBody} {Subject} {Name} {ToAddress}", msg.HtmlBody ?? msg.TextBody, msg.Subject, msg.To.Mailboxes.FirstOrDefault()?.Address ?? "Anon");
             }
             catch (Exception ex)
             {
-                log.LogError(ex, "Error parsing email {Label} {RequestBody}", "parser", requestBody);
+                log.LogError(ex, "Error parsing email {RequestBody}", requestBody);
             }
 
-            await Task.Delay(100, token);
+            await Task.Delay(100);
             return new OkResult();
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Simplz.Loki.Relay
+
+Code for AWS Lamda function, requires SNS Topic if you need the email content
+
+```python
+import json
+import urllib.request
+
+def lambda_handler(event, context):
+    url = "https://demo"
+    message = event['Records'][0]['Sns']['Message']
+    data = message.encode('utf8')
+    req = urllib.request.Request(url, data=data, headers={'content-type': 'application/json'})
+    response = urllib.request.urlopen(req)
+    return response.read()
+```
